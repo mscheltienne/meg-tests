4625f75c2846cbd9abf90d1701ddbce99ef5d297
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-07-04 10:41:23 +0200

    update stimuli code

--- meg_info/audio/stimuli.py
+++ meg_info/audio/stimuli.py
@@ -2,11 +2,14 @@ from bsl.triggers import ParallelPortTrigger
 from psychopy.clock import wait
 from stimuli.audio import Tone
 
+from ..utils._checks import ensure_int
+from ..utils.logs import logger
 
-def run(address: str, frequency: float) -> None:
+
+def run(address: str, frequency: float, volume: float = 20, n: int = 100) -> None:
     """Run a loop of 100 pure tone auditory stimuli.
 
-    Before each stimuli, a trigger is delviered on the first pin of the DB-25
+    Before each stimuli, a trigger is delivered on the first pin of the DB-25
     parallel port. The trigger is not compensated for the stimuli delay.
     `stimuli.Tone` is used to play the auditory stimuli using ``sounddevice``.
 
@@ -17,15 +20,21 @@ def run(address: str, frequency: float) -> None:
     frequency : float
         Frequency of the auditory stimuli. The frequency should be chosen based
         on the sampling rate selected on the DACQ.
+    volume : float
+        Volume at which the sound is played, between 0 and 100.
+    n : int
+        Number of sound repetition.
     """
+    n = ensure_int(n, "n")
+    # error checking on the arguments is handled by BSL and stimuli directly
     trigger = ParallelPortTrigger(address)
-    sound = Tone(volume=20, frequency=frequency)
+    sound = Tone(volume=volume, frequency=frequency, duration=0.2)
 
-    n = 1
-    while n <= 100:
+    for k in range(1, n + 1, 1):
+        logger.info("Event %i / %i", k, n)
         trigger.signal(1)
         sound.play()
-        wait(1, hogCPUperiod=0.2)
+        wait(0.5, hogCPUperiod=0.2)
 
     del sound
     del trigger
--- meg_info/utils/logs.py
+++ meg_info/utils/logs.py
@@ -5,7 +5,7 @@ from typing import Callable, Optional, Union
 
 from ._checks import check_verbose
 from ._docs import fill_doc
-from ._fixes import _WrapStdOut
+from ._fixes import WrapStdOut
 
 
 @fill_doc
@@ -30,7 +30,7 @@ def _init_logger(*, verbose: Optional[Union[bool, str, int]] = None) -> logging.
     logger.setLevel(verbose)
 
     # add the main handler
-    handler = logging.StreamHandler(_WrapStdOut())
+    handler = logging.StreamHandler(WrapStdOut())
     handler.setFormatter(_LoggerFormatter())
     logger.addHandler(handler)
 
@@ -65,8 +65,8 @@ def add_file_handler(
 
 
 @fill_doc
-def set_log_level(verbose: Union[bool, str, int, None]) -> None:
-    """Set the log level for the logger and the first handler ``sys.stdout``.
+def set_log_level(verbose: Optional[Union[bool, str, int]]) -> None:
+    """Set the log level for the logger.
 
     Parameters
     ----------
@@ -99,8 +99,7 @@ class _LoggerFormatter(logging.Formatter):
         super().__init__(fmt="%(levelname): %(message)s")
 
     def format(self, record: logging.LogRecord):
-        """
-        Format the received log record.
+        """Format the received log record.
 
         Parameters
         ----------
