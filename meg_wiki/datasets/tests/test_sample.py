727adb21c71bff776ece66c335eadefb574ccdae
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-07-28 17:23:30 +0200

    improve datasets tests

--- meg_wiki/datasets/tests/test_sample.py
+++ meg_wiki/datasets/tests/test_sample.py
@@ -1,10 +1,15 @@
 from importlib.resources import files
 
 import pooch
+import pytest
 
 from meg_wiki.datasets import sample
 
 
+@pytest.mark.skipif(
+    not (files("meg_wiki").parent / "datasets").exists(),
+    reason="datasets directory is missing",
+)
 def test_sample(tmp_path):
     """Test download of the sample dataset."""
     path = sample.data_path()  # retrieve dataset
@@ -24,4 +29,17 @@ def test_sample(tmp_path):
     assert pooch.file_hash(output) == pooch.file_hash(registry)
     output = tmp_path / "registry-2.txt"
     sample._make_registry(output)
+    assert pooch.file_hash(output) == pooch.file_hash(registry)
+
+
+@pytest.mark.skipif(
+    not (files("meg_wiki").parent / "datasets").exists(),
+    reason="datasets directory is missing",
+)
+def test_registry_up_to_date(tmp_path):
+    """Test that the registry is up to date."""
+    folder = files("meg_wiki").parent / "datasets"
+    output = tmp_path / "registry.txt"
+    pooch.make_registry(folder, output=output, recursive=True)
+    registry = files("meg_wiki.datasets") / "sample-registry.txt"
     assert pooch.file_hash(output) == pooch.file_hash(registry)
