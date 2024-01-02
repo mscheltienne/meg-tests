2983c2320e57c73caa17c9f93687a055462d0752
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2024-01-02 15:00:40 +0100

    rm outdated tests

--- meg_wiki/tests/test_eeg.py
+++ meg_wiki/tests/test_eeg.py
@@ -1,6 +1,5 @@
 from __future__ import annotations  # c.f. PEP 563, PEP 649
 
-from importlib.resources import files
 from typing import TYPE_CHECKING
 
 import pytest
@@ -13,7 +12,6 @@ from meg_wiki.eeg import (
     load_mapping_64chs,
     load_mapping_128chs,
 )
-from meg_wiki.utils._tests import sha256sum
 
 if TYPE_CHECKING:
     from typing import Callable
@@ -21,22 +19,14 @@ if TYPE_CHECKING:
 directory = sample.data_path() / "eeg-layout"
 
 
-@pytest.mark.parametrize("n", (32, 64, 128))
-def test_mapping(n: int) -> None:
-    """Test that all 3 mapping match the standard 10/05 naming."""
-    fname = directory / f"mapping-eeg-{n}-chs.txt"
-    mapping = load_mapping(fname)
-    info = create_info(list(mapping.values()), 1000, "eeg")
-    info.set_montage("standard_1005")
-
-
 @pytest.mark.parametrize(
     "n, func",
     [(32, load_mapping_32chs), (64, load_mapping_64chs), (128, load_mapping_128chs)],
 )
-def test_mapping_sync(n: int, func: Callable) -> None:
-    """Test that the asset folder is in sync with the sample dataset."""
+def test_mapping(n: int, func: Callable[[], dict[str, str]]) -> None:
+    """Test that all 3 mapping match the standard 10/05 naming."""
     fname = directory / f"mapping-eeg-{n}-chs.txt"
-    assert load_mapping(fname) == func()
-    fname2 = files("meg_wiki") / "assets" / "eeg-layout" / f"mapping-eeg-{n}-chs.txt"
-    assert sha256sum(fname) == sha256sum(fname2)
+    mapping = load_mapping(fname)
+    assert mapping == func()
+    info = create_info(list(mapping.values()), 1000, "eeg")
+    info.set_montage("standard_1005")
--- meg_wiki/utils/_tests.py
+++ meg_wiki/utils/_tests.py
@@ -1,6 +1,5 @@
 from __future__ import annotations  # c.f. PEP 563, PEP 649
 
-import hashlib
 from importlib import import_module
 from typing import TYPE_CHECKING
 
@@ -8,17 +7,6 @@ import pytest
 
 if TYPE_CHECKING:
     from typing import Callable
-
-
-def sha256sum(fname):
-    """Efficiently hash a file."""
-    h = hashlib.sha256()
-    b = bytearray(128 * 1024)
-    mv = memoryview(b)
-    with open(fname, "rb", buffering=0) as file:
-        while n := file.readinto(mv):
-            h.update(mv[:n])
-    return h.hexdigest()
 
 
 def requires_module(name: str):  # pragma: no cover
