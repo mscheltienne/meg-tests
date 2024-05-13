0d524fda684fc3addc228c77c47eb62bebf4e343
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-05-13 17:44:42 +0200

    Revert "faster build"
    This reverts commit cd64f3972e991b6de13f90c8bd3cca75f02533a9.

--- tutorials/30_cHPI.py
+++ tutorials/30_cHPI.py
@@ -31,9 +31,17 @@ software on the :ref:`DANA <data-analysis-pc:MEGIN's software>` or
 
 from pathlib import Path
 
+from matplotlib import pyplot as plt
+from mne.chpi import (
+    compute_chpi_amplitudes,
+    compute_chpi_locs,
+    compute_head_pos,
+    filter_chpi,
+)
 from mne.datasets import fetch_fsaverage
 from mne.io import read_raw_fif
-from mne.viz import plot_alignment, set_3d_view
+from mne.preprocessing import maxwell_filter
+from mne.viz import plot_alignment, plot_head_positions, set_3d_view
 
 from meg_wiki.datasets import sample
 
@@ -79,3 +87,135 @@ fig = plot_alignment(
     show_axes=True,
 )
 set_3d_view(fig, 35, 70, distance=0.6, focalpoint=(0.0, 0.0, 0.0))
+
+# %%
+# continuous Head Position Indicator (cHPI)
+# -----------------------------------------
+#
+# If the recording is short, if the participant behaves and is focused on the task, one
+# can assume that the head remained still during the recording. In this case, the
+# initial device to head transformation estimated during the HPI measurement can be
+# assumed to be constant.
+#
+# cHPI is a technique to continuously monitor the head position during the recording,
+# useful when the previous assumption does not hold. When cHPI is enabled, the 5 HPI
+# coils are continuously excited at 5 unique frequencies. The magnetic field produced by
+# the coils is measured by the MEG sensors alongside brain activity.
+
+fig = raw.plot(
+    duration=4,
+    n_channels=10,
+    scalings=dict(grad=4e-10),
+    show_scrollbars=False,
+    show_scalebars=False,
+)
+fig.axes[0].axvline(1.492, color="darkgreen", linestyle="--")
+fig.axes[0].text(
+    1.492 - 0.992, fig.axes[0].get_ylim()[0] * 0.05, "cHPI OFF", fontsize=18
+)
+fig.axes[0].text(1.492 + 1.1, fig.axes[0].get_ylim()[0] * 0.05, "cHPI ON", fontsize=18)
+plt.show()
+
+# %%
+
+raw.compute_psd(fmin=280, fmax=340).plot()
+plt.show()
+
+# %%
+# The signal produced by the HPI coils can be used to estimate the position of the 5
+# emitting coils at different time points, and thus to track the time-varying head
+# position.
+
+# %%
+# Estimating the head position
+# ----------------------------
+#
+# In `MNE-Python <mne stable_>`_, the head position is estimated through 3 steps:
+#
+# - Estimate the cHPI amplitudes with :func:`mne.chpi.compute_chpi_amplitudes`
+# - Estimate the cHPI positions with :func:`mne.chpi.compute_chpi_locs`
+# - Estimate the head position with :func:`mne.chpi.compute_head_pos`
+#
+# .. note::
+#
+#     The function :func:`mne.chpi.compute_head_pos` returns an array of shape
+#     ``(n_pos, 10)`` containing the translations as ``(x, y, z)`` and the rotations as
+#     quaternions ``(q1, q2, q3)``. The translations and rotations can be converted to
+#     translation and rotation matrixes with :func:`mne.chpi.head_pos_to_trans_rot_t`.
+
+chpi_amplitudes = compute_chpi_amplitudes(raw)
+chpi_locs = compute_chpi_locs(raw.info, chpi_amplitudes)
+head_pos = compute_head_pos(raw.info, chpi_locs)
+
+# %%
+# The estimated head position can be plotted in function of time with
+# :func:`mne.viz.plot_head_positions`.
+
+plot_head_positions(head_pos, mode="traces")
+plot_head_positions(head_pos, mode="field")
+plt.show()
+
+# %%
+# Compensate head movements
+# -------------------------
+#
+# Now that the head position at different timepoints, this information can be used to
+# compensate for the head movements during the recording. The function
+# :func:`mne.preprocessing.maxwell_filter` uses the returned head positions in the
+# argument ``head_pos`` to apply the compensation and estimate the signal at every
+# sensor position as if the head was still, i.e., in the position measured by the
+# initial HPI measurement.
+#
+# .. note::
+#
+#     It is important to first remove the cHPI signal from the recording and to mark
+#     bad channels.
+
+raw = filter_chpi(raw)
+raw.info["bads"] = [  # strict selection for this example
+    "MEG1531",
+    "MEG1532",
+    "MEG1533",
+    "MEG1542",
+    "MEG1522",
+    "MEG1541",
+    "MEG1321",
+]
+calibration = sample.data_path() / "calibration" / "sss_cal.dat"
+cross_talk = sample.data_path() / "cross-talk" / "ct_sparse.fif"
+raw_sss = maxwell_filter(
+    raw, calibration=calibration, cross_talk=cross_talk, head_pos=head_pos
+)
+
+# %%
+# Let's visualize the effect on a triplet of sensors located on the top of the head.
+# Note however that this represents the effect of SSS and movemvent compensation
+# together.
+
+f, ax = plt.subplots(3, 1, sharex=True, figsize=(10, 5), layout="constrained")
+times = raw.times[raw.time_as_index(30)[0] :]
+picks = ["MEG0711", "MEG0712", "MEG0713"]
+for k, pick in enumerate(picks):
+    data = raw.get_data(picks=pick, tmin=30).squeeze()
+    ax[k].plot(times, data, label="Raw" if k == 0 else None)
+    data = raw_sss.get_data(picks=pick, tmin=30).squeeze()
+    ax[k].plot(times, data, color="red", label="SSS" if k == 0 else None)
+    ax[k].set_title(f"{'MAG:' if k == 0 else 'GRAD:'} {pick}")
+plt.show()
+
+# %%
+# Magnetometers which are more sensitive to distant fields and thus to environmental
+# artifacts are more affected by SSS than gradiometers.
+#
+# .. note::
+#
+#     Note that on top of movement compensation, SSS can also transform the signal as if
+#     it was emitted from a different head position, through the argument
+#     ``destination``. This can be useful to compare signal between subjects in sensor
+#     space or to bring back a tilted head to a more upright position.
+
+# %%
+# References
+# ----------
+#
+# .. footbibliography::
